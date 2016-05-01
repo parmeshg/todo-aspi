74ba2bfafb7f74eef4cd440e0b02651d0cb08ea5
Parmesh Ghanvat <Parmesh.Ghanvat@sigma-systems.com>
2016-05-02 01:21:30 +0530

    Added variable playground and updated server to add query

--- server.js
+++ server.js
@@ -14,8 +14,17 @@ app.get('/',function (res,res){
 	res.send('TODO API root');
 });
 
+//GET /todos?completed=true
 app.get('/todos',function (req,res){
-	res.json(todos);	
+	var queryParams = req.query;
+	var filteredTodos = todos;
+
+	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true'){
+		filteredTodos = _.where(filteredTodos, {completed: true});
+	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false'){
+		filteredTodos = _.where(filteredTodos, {completed: false});
+    }
+	res.json(filteredTodos);
 });
 
 app.get('/todos/:id',function (req,res){
@@ -67,6 +76,31 @@ app.delete('/todos/:id',function (req,res){
 	}
 });
 
+app.put('/todos/:id',function (req,res){
+    var todoId = parseInt(req.params.id,10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+	var body = _.pick(req.body, 'description', 'completed' );
+    var validAttributes = {};
+
+	if(!matchedTodo)  {
+		return res.status(404).send();
+	}
+
+	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)){
+		validAttributes.completed = body.completed;
+	} else if (body.hasOwnProperty('completed')){
+		return res.status(400).send();
+	}
+
+	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0){
+		validAttributes.description = body.description;
+	} else if (body.hasOwnProperty('description')){
+		return res.status(400).send();
+	}
+
+	_.extend(matchedTodo, validAttributes);
+	res.json(matchedTodo);
+});
 
 app.listen(PORT, function (){
 	console.log('TODO API web server is started on ' + PORT);
