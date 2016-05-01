7b2620db0f5a0f370a6d819a42be0e9675453e42
Parmesh Ghanvat <Parmesh.Ghanvat@sigma-systems.com>
2016-05-01 23:01:38 +0530

    Refactor with use of underscore and validation

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+var _ = require("underscore");
+
 var PORT = process.env.PORT || 3000;
 var todos= [];
 var todoNextId = 1;
+
 
 app.use(bodyParser.json());
 
@@ -17,13 +20,14 @@ app.get('/todos',function (req,res){
 
 app.get('/todos/:id',function (req,res){
 	var todoId = parseInt(req.params.id,10);
-	var matchedTodo;
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+	/*var matchedTodo;
 
 	todos.forEach(function (todo){
 		if ( todoId === todo.id ){
 			matchedTodo = todo;			
 		}
-	});
+	});*/
 
 	if (matchedTodo){
 		res.json(matchedTodo);		
@@ -34,10 +38,18 @@ app.get('/todos/:id',function (req,res){
 
 // POST /todos
 app.post('/todos',function (req,res){
+	var body = _.pick(req.body, 'description', 'completed' );
+
+	if(!_.isBoolean(body.completed) || !_.isString(body.description)  || body.description.trim().length === 0)  {
+		return res.status(400).send();
+	}
+	body.description = body.description.trim();
+	/*
 	var body = req.body;
 	console.log('todoNextId: ' + todoNextId);
 	console.log('description: ' + body.description);
 	console.log('completed: ' + body.completed);
+	*/
 	body.id = todoNextId++;
 	todos.push(body);
 	res.json(body);
