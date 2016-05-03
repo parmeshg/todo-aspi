7e4f1cecb1129b15e78dd8518602a1c7aa56363b
Parmesh Ghanvat <Parmesh.Ghanvat@sigma-systems.com>
2016-05-03 18:44:30 +0530

    Convert CRUD request to use current user todos only

--- server.js
+++ server.js
@@ -19,7 +19,9 @@ app.get('/',function (res,res){
 //GET /todos?completed=true&q=work
 app.get('/todos',middleware.requireAuthentication,function (req,res){
 	var query = req.query;
-    var where = {};
+    var where = {
+    	userId: req.user.get('id')
+    };
     
     if (query.hasOwnProperty('completed') && query.completed === 'true') {
     	where.completed = true;
@@ -44,7 +46,13 @@ app.get('/todos',middleware.requireAuthentication,function (req,res){
 app.get('/todos/:id',middleware.requireAuthentication,function (req,res){
 	var todoId = parseInt(req.params.id,10);
 	
-	db.todo.findById(todoId).then(function (todo) {
+	db.todo.findOne({
+		where:{
+			id:todoId,
+			userId: req.user.get('id')
+			}
+		}
+		).then(function (todo) {
 		if(!!todo){			
 			res.json(todo.toJSON());
 		} else {			
@@ -90,7 +98,8 @@ app.delete('/todos/:id',middleware.requireAuthentication,function (req,res){
 
 	db.todo.destroy({
 		where:{
-			id: todoId
+			id: todoId,
+			userId: req.user.get('id')
 		}
 	}).then ( function (rowsDeleted) {
 		if(rowsDeleted === 0) {
@@ -116,7 +125,12 @@ app.put('/todos/:id',middleware.requireAuthentication,function (req,res){
 		attributes.description = body.description;
 	}
 	
-	db.todo.findById(todoId).then(function(todo) {
+	db.todo.findOne({
+		where:{
+			id: todoId,
+			userId: req.user.get('id')
+		}
+	}).then(function(todo) {
 	if (todo) {
 			todo.update(attributes).then( function (todo) {
 		res.json(todo.toJSON());		
@@ -160,7 +174,7 @@ app.post('/users/login',function (req,res){
 
 
 db.sequelize.sync(
-	{force: true}
+	//{force: true}
 	).then( function () {
 	app.listen(PORT, function (){
 	console.log('TODO API web server is started on ' + PORT);
